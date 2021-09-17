a4faf3d0e7f0f2943bcc0e36aaf2c18115cf2839
Aashish <aasidpandey@gmail.com>
2021-09-17 10:33:45 +0545

    File Upload

---
+++ index.js
@@ -0,0 +1,60 @@
+function myfunction_one(){
+  document.getElementById('services').style.display="flex";
+  document.getElementById('vas').style.display="none";
+  document.getElementById('organization').style.display="none";
+  document.getElementById('nettv').style.display="none";
+  document.getElementById('bnp').style.display="none";
+  document.getElementById('help').style.display="none";
+  document.getElementById('one').style.marginLeft="3.5vw";
+}
+function myfunction_two(){
+  document.getElementById('services').style.display="none";
+  document.getElementById('vas').style.display="none";
+  document.getElementById('organization').style.display="none";
+  document.getElementById('nettv').style.display="flex";
+  document.getElementById('bnp').style.display="none";
+  document.getElementById('help').style.display="none";
+  document.getElementById('two').style.marginLeft="9vw";
+
+}
+function myfunction_three(){
+  document.getElementById('organization').style.display="none";
+  document.getElementById('nettv').style.display="none";
+  document.getElementById('vas').style.display="flex";
+  document.getElementById('services').style.display="none";
+  document.getElementById('bnp').style.display="none";
+  document.getElementById('help').style.display="none";
+  document.getElementById('three').style.marginLeft="17.5vw";
+
+}        
+function myfunction_four(){
+  document.getElementById('nettv').style.display="none";
+  document.getElementById('vas').style.display="none";
+  document.getElementById('services').style.display="none";
+  document.getElementById('organization').style.display="flex";
+  document.getElementById('bnp').style.display="none";
+  document.getElementById('help').style.display="none";
+  document.getElementById('four').style.marginLeft="29vw";
+}
+
+function myfunction_five(){
+  document.getElementById('nettv').style.display="none";
+  document.getElementById('vas').style.display="none";
+  document.getElementById('services').style.display="none";
+  document.getElementById('organization').style.display="none";
+  document.getElementById('bnp').style.display="flex";
+  document.getElementById('help').style.display="none";
+  document.getElementById('five').style.marginLeft="39vw";
+}
+
+function myfunction_six(){
+  document.getElementById('nettv').style.display="none";
+  document.getElementById('vas').style.display="none";
+  document.getElementById('services').style.display="none";
+  document.getElementById('organization').style.display="none";
+  document.getElementById('bnp').style.display="none";
+  document.getElementById('help').style.display="flex";
+  document.getElementById('six').style.marginLeft="46.5vw";
+
+
+}
